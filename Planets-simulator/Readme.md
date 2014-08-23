a23564ddbcda2e4ed3e8b9fc9d4cd4aafc195e29
gabrielfougeron <gabriel.fougeron@hotmail.fr>
2014-08-23 22:41:26 +0200

    deleting files

---
+++ Planets-simulator/Readme.md
@@ -0,0 +1,1 @@
+This independant Fortran program integrates the N-body Kepler problem usint either an explicit or an implicit Runge-Kutta method.
---
+++ Planets-simulator/main.f90
@@ -0,0 +1,15 @@
+! Fortran 90 program performing Runge-Kutta steps on the Kepler N-body problem
+
+program main
+
+    implicit none
+    
+    include 'def.f90'
+    include 'prog.f90'
+    
+contains
+
+    include 'functions.f90'
+    
+end program main
+
